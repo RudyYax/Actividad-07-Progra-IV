f02fad12583d6cdca1ab518d200deb01f50b63c6
Rudy <rudyax2004@gmail.com>
2025-07-16 19:04:26 -0600

    Commit Principal

--- Actividad7.py
+++ Actividad7.py
@@ -1,29 +1,63 @@
 estudiantes = {}
-print("Actividad 07")
-print("-----Bienvenidos-----")
-print("<<<<REGISTRO ESTUDIANTES UNIVERSITARIOS>>>>")
-print("1.- Registrar estudiante")
-print("2.- Mostrar todos los estudiantes y sus curos")
-print("3.- Buscar estudiante por numero de carnet")
-print("4.- Salir")
-opcion = int(input("Ingrese la opcion que desee: "))
+opcion = 0
+while opcion != 4:
+    print("Actividad 07")
+    print("-----Bienvenidos-----")
+    print("<<<<REGISTRO ESTUDIANTES UNIVERSITARIOS>>>>")
+    print("1.- Registrar estudiante")
+    print("2.- Mostrar todos los estudiantes y sus curos")
+    print("3.- Buscar estudiante por numero de carnet")
+    print("4.- Salir")
+    opcion = int(input("Ingrese la opcion que desee: "))
+    match(opcion):
+        case 1:
+            print("Bienvenido")
+            print("Registrar estudiante")
+            cantidad = int(input(f" \n Ingese Cantidad de estudiantes: "))
+            for i in range(cantidad):
+                print(f" \n Estudiante {i + 1}")
+                Carnet = input("Ingrese carnet del estudiante: ")
+                if Carnet in estudiantes:
+                    print("El carnet ya esta ingresado no puede repetir un ingreso")
+                else:
+                    Nombre = input("Ingrese el nombre completo del estudiante: ")
+                    Edad = input("Ingrese la edad del estudiante: ")
+                    Carrera = input("Ingrese la carrera del estudiante: ")
+                    CantidadCuros = input("Ingrese la cantidad de curos que deseea inscribir: ")
+                    for i in range(CantidadCuros):
+                        print(f"\n Curso {i + 1}")
+                        CodigoCurso = input("Ingrese el codigo del curso")
+                        if CodigoCurso in estudiantes:
+                            print("El carnet ya esta ingresado no puede repetir un ingreso")
+                        else:
+                            NombreCurso = input("Ingrese el nombre del curso que deseea inscribir: ")
+                            NotaTarea = input("Ingrese la nota de Tareas")
+                            if NotaTarea < 0:
+                                print("La nota no puede ser negativa")
+                            else:
+                                NotaParcial = input("Ingrese la nota de los parciales")
+                                if NotaParcial < 0:
+                                    print("La nota no puede ser negativa")
+                                else:
+                                    NotaProyecto = input("Ingrese la nota de la proyecto")
+                                    if NotaProyecto < 0:
+                                        print("La nota no puede ser negativa")
+                                    else:
+                                        print("Estudiantes Registrados correctamente")
+            estudiantes[Carnet] = {
+                "Nombre": Nombre,
+                "Edad": Edad,
+                "Carrera": Carrera,
+                "CursosTotales" : {
+                    "NombreCurso": NombreCurso,
+                    "NotaParcial": NotaParcial,
 
-match(opcion):
-    case 1:
-        print("Bienvenido")
-        print("Registrar estudiante")
-        cantidad = int(input(f" \n Ingese Cantidad de estudiantes: "))
-        for i in range(cantidad):
-            print(f" \n Estudiante {i + 1}")
-            Carnet = input("Ingrese carnet del estudiante: ")
-            Nombre = input("Ingrese el nombre completo del estudiante: ")
-            Edad = input("Ingrese la edad del estudiante: ")
-            Carrera = input("Ingrese la carrera del estudiante: ")
-            CantidadCuros = input("Ingrese la cantidad de curos que deseea inscribir: ")
-            for i in range(CantidadCuros):
-                print(f"\n Curso {i + 1}")
-                NombreCurso = input("Ingrese el nombre del curso que deseea inscribir: ")
-                NotaTarea =
+                }
+
+            }
+
+
+
 
 
 
